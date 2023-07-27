c7f7ba2aa488c6f7b270bfbad34728fa6c6bba02
Irving <Irving@timepercentcorp.com>
2023-07-27 17:11:36 +0900

    Add Bootstarp

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 // npm run dev
 
 import './app.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import App from './App.svelte'
 
 const app = new App({

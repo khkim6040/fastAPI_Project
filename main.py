035d7781929b94a3b89abc7cd545572ea67015c2
Irving <Irving@timepercentcorp.com>
2023-07-27 19:40:21 +0900

    trivial

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from domain.user import user_router
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:5173", "http://localhost:5173"
+    "http://127.0.0.1:5173", "http://localhost:5173",
+    "http://127.0.0.1:8000", "http://localhost:8000",
+    "http://13.209.184.143:8000",
 ]
 
 app.add_middleware(
